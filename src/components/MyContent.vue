6760ada2eeaaffbc15c7569a38affc422055a5eb
Kardast <dottorsf@gmail.com>
2022-06-14 16:27:58 +0200

    Printing on page some infos

--- src/components/MyContent.vue
+++ src/components/MyContent.vue
@@ -3,22 +3,56 @@
 
     <MySearchbar/>
 
-    <ul>
-      <li>prova 1
-      </li>
-    </ul>
+    <MyContentCard
+      v-for="(item, index) in myArrayList"
+      :key="index"
+      :contentObject="item"
+    />
   </div>
 </template>
 
 <script>
-import MySearchbar from './MySearchbar.vue'
+import axios from "axios";
+import MySearchbar from './MySearchbar.vue';
+import MyContentCard from './MyContentCard.vue';
+
 export default {
   name: 'MyContent',
   components: {
-      MySearchbar
+      MySearchbar,
+      MyContentCard
   },
-  props: {
 
+// My API, array and what the user searches
+  data(){
+    return {
+      apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=ae88472fec19f34c8ca3084cba86594c&query=ritorno+al+futuro&language=it-IT",
+      myArrayList: [],
+      userText: ""
+    }
+  },
+
+  created(){
+    this.getList()
+  },
+
+  methods: {
+    // axios call
+    getList(){
+      axios
+      .get(this.apiUrl)
+      .then((result) => {
+        this.myArrayList = result.data.results;
+        console.log(this.myArrayList);
+      })
+      .catch((error) => {
+        console.log("Errore", error);
+      })
+    },
+
+    // searchContent(userInput){
+    //   this.userText = userInput;
+    // }
   }
 }
 </script>
---
+++ src/components/MyContentCard.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div class="my-content-card">
+
+    <ul>
+
+      <li>{{contentObject.title}}</li>
+      <li>{{contentObject.original_title}}</li>
+      <li>{{contentObject.original_language}}</li>
+      <li>{{contentObject.vote_average}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyContentCard',
+  props: {
+    contentObject: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.my-content-card{
+    color: white;
+}
+</style>
